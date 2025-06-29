40a69405fb36d70609d05ca2281036b7e79178e6
jatin9761 <sharmajatin0085@gmail.com>
2025-06-29 23:53:45 +0530

    new

---
+++ frontend/src/home/Left/Left.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function left() {
+  return (
+    <div className='border border -black-500 w-1/4 h-screen bg-gray-100 p-4'>
+      left
+    </div>
+  )
+}
---
+++ frontend/src/home/Right/Right.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Right() {
+  return (
+    <div className='border border-black'>
+      Right
+    </div>
+  )
+}
