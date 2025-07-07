ef92d1760db7e89152fef5093ace505e4af4f3e9
jatin9761 <sharmajatin0085@gmail.com>
2025-07-07 23:26:24 +0530

    new

--- frontend/src/home/Right/Message.jsx
+++ frontend/src/home/Right/Message.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-function message() {
+function Message() {
   return (
-    <>
-      <div>
-        <div className="chat chat-end">
-          <div className="chat-bubble chat-bubble-info">hii </div>
-        </div>
-
-        <div className="chat chat-start">
-          <div className="chat-bubble chat-bubble-secondary"> bye</div>
-        </div>
+    <div className="flex-grow p-4 space-y-3 overflow-y-auto">
+      <div className="chat chat-start">
+        <div className="chat-bubble bg-pink-400 text-black">hii !</div>
       </div>
-    </>
+      <div className="chat chat-end">
+        <div className="chat-bubble bg-sky-300 text-black">kon ?</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble bg-sky-400 text-black">aap ka dost</div>
+      </div>
+    </div>
   );
 }
 
-export default message;
+export default Message;
--- frontend/src/home/Right/type.jsx
+++ frontend/src/home/Right/type.jsx
@@ -6,12 +6,14 @@ function type() {
     <>
       <div className='flex space-x-3 h-[6vh] text-center bg-gray-600'>
         <div className="w-[90%] mx-15 ">
-        <input type="text" placeholder="Type here" className="border-[1px] border border-gray-700 bg-slate-900  flex items-center w-full py-3 px-3 rounded-xl grow outline-none bg-slate-900 mt-1 " />
+        <input type="text" 
+        placeholder="Type here" 
+        className="border-[1px] border border-gray-700 bg-slate-900  flex items-center w-full py-3 px-3 rounded-xl grow outline-none bg-slate-900 mt-1 " />
 
         
       </div>
-      <button className='text-3xl'>
-            <VscSend />
+      <button>
+            <VscSend  className='text-3xl' />
         </button>
       </div>
     </>
