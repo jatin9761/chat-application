745dc513e20f374c21211a5e68838773cb9ac899
jatin9761 <sharmajatin0085@gmail.com>
2025-07-09 23:34:05 +0530

    new

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+
 const app = express()
 dotenv.config();
 const PORT = process.env.PORT || 5002;
+const URI =process.env.MONGODB_URI;
+
+try{
+  mongoose.connect(URI);
+  console.log("MongoDB connected"); 
+
+}catch (error){
+ console.log(error);
+}
 
 app.get('/', (req, res) => {
   res.send('my name is jatin sharma and i am a full stack developer');
